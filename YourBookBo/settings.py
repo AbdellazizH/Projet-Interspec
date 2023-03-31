e10ccbb3e248ffb40bd10d9c7b5a40a773fa7b54
Aziz <doukali_fr@live.fr>
2023-03-31 11:51:30 +0200

    configuration de secret key dans le fichier .env

--- YourBookBo/settings.py
+++ YourBookBo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^$n-fcf8eygr!(izljx$^=rpy%*6cb45g%05@y#vpbi3tvm^f'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
